a9263810e2c34cf4c9ed9bcb146ff586fdaca8c5
A-Krittanat <krittanat.ananchailikit@scb.co.th>
2020-05-16 01:15:36 +0700

    Loan controller test

--- src/test/java/com/digitalacademy/loan/service/LoanServiceTest.java
+++ src/test/java/com/digitalacademy/loan/service/LoanServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
