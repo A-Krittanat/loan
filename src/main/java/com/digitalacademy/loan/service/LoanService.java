1ecabc78b8d408cb5d27aed7588dabfa0f7eec89
A-Krittanat <krittanat.ananchailikit@scb.co.th>
2020-05-15 14:34:22 +0700

    Loan service test

--- src/main/java/com/digitalacademy/loan/service/LoanService.java
+++ src/main/java/com/digitalacademy/loan/service/LoanService.java
@@ -1,13 +1,14 @@
 package com.digitalacademy.loan.service;
 
+import com.digitalacademy.loan.constants.LoanError;
 import com.digitalacademy.loan.controller.LoanController;
+import com.digitalacademy.loan.exception.LoanException;
 import com.digitalacademy.loan.model.LoanInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class LoanService {
@@ -24,6 +25,12 @@ public class LoanService {
             loanInfo.setAccountPayable("102-220-2200");
             loanInfo.setAccountReceivable("102-210-2020");
             loanInfo.setPrincipalAmount(3200.00);
+        } else if(id.equals(2L)) {
+            log.info("id: {}", id);
+            throw new LoanException(
+                    LoanError.GET_LOAN_INFO_NOT_FOUND,
+                    HttpStatus.BAD_REQUEST
+            );
         } else {
             log.info("id: {}", id);
             throw new Exception("Test throw new exception");
